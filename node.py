140216347c557a6edc2a2ccc8e130283dbae4a88
Zelazny7 <gravcon5@gmail.com>
2013-02-14 21:32:17 -0600

    created node class

---
+++ node.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from interaction import Interaction, Split
+from io import BytesIO
+from lxml import etree
+from lxml.etree import tostring
+from pandas import Series, DataFrame
+
+class Node(object):
+    node_type = 'root'
+    split_attr = None
+    split_val  = None
+    side = None
+
+    def __init__(self, parent, type):
+        self.parent = parent
+        pass
